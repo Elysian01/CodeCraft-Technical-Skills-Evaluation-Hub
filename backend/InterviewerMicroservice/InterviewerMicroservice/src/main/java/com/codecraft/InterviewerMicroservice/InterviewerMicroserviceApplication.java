689bb828d05dd18a704f16febcd02163649afe5d
Abhishek Gupta <abhig0209@gmail.com>
2024-05-14 12:47:01 +0530

    project restarted

---
+++ backend/InterviewerMicroservice/InterviewerMicroservice/src/main/java/com/codecraft/InterviewerMicroservice/InterviewerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.codecraft.InterviewerMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewerMicroserviceApplication.class, args);
+	}
+
+}
